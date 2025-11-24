9509f08a046c525e313f87d134ae3d060a7ded63
Shaik Rehan Basha <119519167+Shaikbasha1301@users.noreply.github.com>
2025-11-24 19:47:12 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 resource "aws_launch_template" "web_server_as" {
-    name = "myproject"
-    image_id           = "ami-0cae6d6fe6048ca2c"
+    name = "rbproject"
+    image_id           = "ami-0fa3fe0fa7920f68e"
     vpc_security_group_ids = [aws_security_group.web_server.id]
     instance_type = "t3.micro"
-    key_name = "minimajor"
+    key_name = "RBKEY"
     tags = {
-        Name = "DevOps"
+        Name = "RBDevOps"
     }
     
 }
@@ -15,7 +15,7 @@ resource "aws_launch_template" "web_server_as" {
   resource "aws_elb" "web_server_lb"{
      name = "web-server-lb"
      security_groups = [aws_security_group.web_server.id]
-     subnets = ["subnet-097b4975cf1130493", "subnet-01ee7e5094f09f366"]
+     subnets = ["subnet-0e2c764aa541cfacc", "subnet-08c395a847e4c2583"]
      listener {
       instance_port     = 8000
       instance_protocol = "http"
